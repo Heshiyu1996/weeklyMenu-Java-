9eaf4b87173aa3fdccf2db7b6c0245de92454970
你的宇哥哥 <450979309@qq.com>
2018-02-26 14:31:33 +0800

    新增注销接口

--- src/com/controller/StaffController.java
+++ src/com/controller/StaffController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.enterprise.inject.New;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -41,12 +42,17 @@ public class StaffController {
 		String sid=(String)session.getAttribute("sid_session");
 		Map<String,Object> map=new HashMap<String, Object>();
 		if(sid==null){
-			map.put("msg", "请先登录");
+			map.put("success", false);
+			map.put("msg", "Session已过期，请重新登录！");
 		} else {
 			Staff staff = staffService.getStaffBySid(sid);
-			map.put("sid",staff.getsid());
-			map.put("sname", staff.getSname());
-			map.put("smobile", staff.getSmobile());
+			Map<String, Object> userMap=new HashMap<String, Object>();
+			userMap.put("sid",staff.getsid());
+			userMap.put("sname", staff.getSname());
+			userMap.put("smobile", staff.getSmobile());
+			map.put("msg", "获取用户信息");
+			map.put("relatedObject", userMap);
+			map.put("success", true);
 		}
 		return map;
 	}
@@ -66,6 +72,22 @@ public class StaffController {
 		} else {
 			map.put("status", false);
 			map.put("msg", "登录失败");
+		}
+
+		return map;
+	}
+//	退出登录
+	@RequestMapping(value = "/logout.do", method = RequestMethod.GET)
+	public @ResponseBody
+	Map<String, Object> logoutUser(HttpSession session) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		session.setAttribute("sid_session", null);
+		if((String)session.getAttribute("sid_session")==null){
+			map.put("msg", "注销成功");
+			map.put("success", true);
+		}else {
+			map.put("msg", "注销失败");
+			map.put("success", false);
 		}
 
 		return map;
